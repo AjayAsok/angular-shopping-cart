95cae416248cfe82529eba6c7748f8b789c2d8f4
Ajay Asok <ajayasok007@gmail.com>
2021-04-15 02:30:05 +0530

    layout corrections

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,6 +2,13 @@ import { Component } from "@angular/core";
 
 import { products } from "../products";
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: "app-product-list",
   templateUrl: "./product-list.component.html",
@@ -16,10 +23,11 @@ export class ProductListComponent {
   onNotify(name: string) {
     window.alert("You will be notified when the " + name + " goes on sale");
   }
+  tiles: Tile[] = [
+    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
+  ];
 }
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
